68d4c6a1c30bef9960e69c54c5ed0e5ed4b30194
chu-max <60244407+chu-max@users.noreply.github.com>
2020-05-07 10:13:32 -0400

    added some tests, not passing because we need to error check more

--- src/test/java/edu/brown/cs/student/KDTreeTest.java
+++ src/test/java/edu/brown/cs/student/KDTreeTest.java
@@ -9,6 +9,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import edu.brown.cs.student.brown_spotify.Song;
+import edu.brown.cs.student.kdtree.Coordinates;
 import edu.brown.cs.student.kdtree.KdTree;
 import edu.brown.cs.student.kdtree.KdTreeNode;
 
@@ -26,6 +27,42 @@ public class KDTreeTest {
     @Before
     public void setUpTree() {
 	ArrayList<KdTreeNode> nodes = new ArrayList<KdTreeNode>();
+	 // Create set of coordinates
+	    Double[] firstCoord = {
+	        1.0, 2.0, 3.0
+	    };
+	    Double[] secondCoord = {
+	        -4.0, 5.0, 6.0
+	    };
+	    Double[] thirdCoord = {
+	        7.0, -8.0, 9.0
+	    };
+	    
+	    Double[] fourthCoord = {
+	        10.0, 11.0, -12.0
+	    };
+	    
+	    Coordinates firstCoor = new Coordinates(thirdCoord);
+	    Coordinates secondCoor = new Coordinates(thirdCoord);
+	    Coordinates thirdCoor = new Coordinates(thirdCoord);
+	    Coordinates fourthCoor = new Coordinates(thirdCoord);
+	    
+	    /*
+	    KdTreeNode first = new KdTreeNode(null, null, firstCoor, null);
+	    KdTreeNode second = new KdTreeNode("second", secondCoor);
+	    KdTreeNode third = new KdTreeNode("third", thirdCoor);
+	    KdTreeNode fourth = new KdTreeNode("fourth", fourthCoor);
+	   
+	    nodes.add(first);
+	    nodes.add(second);
+	    nodes.add(third);
+	    nodes.add(fourth);
+	    // Populate tree.
+	    tree1 = new KdTree<Song>(nodes, 3);
+	    //not sure what to do about KdComparator 
+	    root1 = tree1.buildTree(null, nodes, 0);
+	    
+	    */
 
     }
 
---
+++ src/test/java/edu/brown/cs/student/UniTunesTest.java
@@ -0,0 +1,24 @@
+package edu.brown.cs.student;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import java.util.ArrayList;
+import org.junit.Before;
+import org.junit.Test;
+import edu.brown.cs.student.kdtree.Coordinates;
+import edu.brown.cs.student.kdtree.KdTree;
+import edu.brown.cs.student.kdtree.KdTreeNode;
+
+
+import edu.brown.cs.student.brown_spotify.Song;
+
+public class UniTunesTest {
+    
+    @Before
+    public void setup() {
+	
+    }
+    
+   
+
+}
